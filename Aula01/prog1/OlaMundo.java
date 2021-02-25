32d539c2e022d1827bcc3137f886308728dd3bd0
Thuti1 <arcasar1@hotmail.com>
2021-02-25 09:24:00 -0300

    1o programa incial, aprender a printar e compilar

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,11 @@
+class OlaMundo{
+    public static void main(String[] args) {
+        //Isso é um comentário de uma linha
+        System.out.println("Ola Mundo!");
+        System.out.println("Stain eh mal?");
+        /*
+        *Comentário de diversas linhas
+        *Continuação
+        */
+    }
+}
