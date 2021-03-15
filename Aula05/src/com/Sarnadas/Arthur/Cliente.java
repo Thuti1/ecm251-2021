df0679a8fafba8468ef6a0ce94ada7277d5150fb
Thuti1 <arcasar1@hotmail.com>
2021-03-15 18:05:50 -0300

    Aula05
    Criação de uma interface, com switch (pouco sustentável à longo prazo) e novas funções.

---
+++ Aula05/src/com/Sarnadas/Arthur/Cliente.java
@@ -0,0 +1,20 @@
+package com.Sarnadas.Arthur;
+
+public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
