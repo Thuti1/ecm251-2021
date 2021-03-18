99ecd0f423a40869bb9b5230372288248396c03b
Thuti1 <arcasar1@hotmail.com>
2021-03-18 09:22:33 -0300

    Aula05 - Teoria
    Validação do CPF a partir do seu dígito

---
+++ Aula05-teoria/src/com/Sarnadas/Arthur/Main.java
@@ -0,0 +1,8 @@
+package com.Sarnadas.Arthur;
+
+public class Main {
+    public static void main(String[] args) {
+        String cpf = "258.469.854-25";
+        System.out.println("CPF válido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
