60375b42c049a7641569fb624b31b17b2d5900bf
Thuti1 <arcasar1@hotmail.com>
2021-04-02 19:30:56 -0300

    Início da implementação dos métodos
    Colocar a ideia inicial dos métodos de receber e enviar dinheiro, com erros na formatação e quanto ao objeto da Conta.

--- Trabalhos/src/Sarnadas/Arthur/Contas.java
+++ Trabalhos/src/Sarnadas/Arthur/Contas.java
@@ -1,10 +1,14 @@
 package Sarnadas.Arthur;
 
+import java.text.NumberFormat;
+import java.util.Random;
+
 public class Contas {
     //Declarar as infos de cada conta
     private Usuários usuario;
     private int idConta;
     private double saldo;
+    private static int R = 0;
 
     //Fazer um construtor da conta
     public Contas(int idConta, double saldo, String nome, String senha, String email) {
@@ -12,25 +16,47 @@ public class Contas {
         this.saldo = saldo;
         this.usuario = new Usuários(nome,senha,email);
     }
-    //Métodos das contas
-    public void depositar(double valor) {
+    //Esses métodos não são utilizáveis pelo usuário
+    private void depositar(double valor) {
         this.saldo += valor;
     }
-    public boolean sacar(double valor) {
-        if(valor <= this.saldo){
-            this.saldo -= valor;
+
+    private boolean sacar(Contas conta, double valor) {
+        if(valor <= conta.saldo){
+            conta.saldo -= valor;
             return true;
         }
         return false;
     }
-    public boolean transferirDinheiro(Contas destino, double valor) {
-        if (this.sacar(valor)) {
+
+    private static int getRandomNumberInRange(int min, int max){
+        Random r = new Random();
+        return r.nextInt((max-min)+1) + min;
+
+        //Métodos das contas
+    public boolean transacao(Contas pagador, Contas recebedor, String T) {
+        String[] dados = T.split(";");
+        recebedor =
+                valor = dados[2];
+        if (sacar(pagador, valor)) {
+            recebedor.depositar(valor);
+        }
+    }
+    /*    if (this.sacar(valor)) {
             destino.depositar(valor);
             return true;
         }
         return false;
     }
+    */
     // Exibição das infos da conta
+    public String receber(Contas recebedor, double valor){
+        //outra possibilidade para passar int pra String = NumberFormat.getInstance().format(num inteiro)
+        String T = Integer.toString(recebedor.idConta) + ";"+recebedor.usuario.nome + Double.toString(valor)+";"+ Integer.toString(getRandomNumberInRange(1000, 9999));
+        return T;
+        }
+
+
     @Override
     public String toString() {
         return "Contas{" +
