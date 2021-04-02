f1444e5dcc5af2f64531317a631e2c05fd3433af
Thuti1 <arcasar1@hotmail.com>
2021-04-02 18:16:10 -0300

    Métodos do banco/ Transações
    Colocar os métodos para fazer cada transação do banco

--- Trabalhos/src/Sarnadas/Arthur/Contas.java
+++ Trabalhos/src/Sarnadas/Arthur/Contas.java
@@ -7,9 +7,28 @@ public class Contas {
     private double saldo;
 
     //Fazer um construtor da conta
-    public Contas(int idConta, double saldo) {
+    public Contas(int idConta, double saldo, String nome, String senha, String email) {
         this.idConta = idConta;
         this.saldo = saldo;
+        this.usuario = new Usuários(nome,senha,email);
+    }
+    //Métodos das contas
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+    public boolean sacar(double valor) {
+        if(valor <= this.saldo){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    public boolean transferirDinheiro(Contas destino, double valor) {
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
     }
     // Exibição das infos da conta
     @Override
