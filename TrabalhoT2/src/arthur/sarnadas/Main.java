53af10c0ae2049e97e124d71a0180f1cef111e11
Thuti1 <arcasar1@hotmail.com>
2021-06-24 17:05:35 -0300

    Menu
    Criação de um menu funcional

--- TrabalhoT2/src/arthur/sarnadas/Main.java
+++ TrabalhoT2/src/arthur/sarnadas/Main.java
@@ -1,6 +1,7 @@
 // Arthur C Sarnadas - 19.00756-6
 // Giovanni B Benuthe - 19.00043-0
 // Luiz F Rodrigues – 19.01358-2
+package arthur.sarnadas;
 
 import arthur.sarnadas.models.Sistema;
 
@@ -8,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 	//Iniciar Sistema:
-        Sistema.Menu();
+        Sistema sis = new Sistema();
+        sis.executar();
     }
 }
--- TrabalhoT2/src/arthur/sarnadas/models/Sistema.java
+++ TrabalhoT2/src/arthur/sarnadas/models/Sistema.java
@@ -3,12 +3,60 @@ package arthur.sarnadas.models;
 // Biblioteca para leitura de .CSV
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Locale;
+import java.util.Scanner;
+import arthur.sarnadas.CLS; //Tentar limpar a tela depois
 
 public class Sistema {
-    public static void Menu(){
+    private boolean executarSistema;
+    private Scanner scanner;
+
+    public void opcao(String escolha){
+        switch(escolha) {
+            case "C":
+                //Cadastra o maluco
+                System.out.println("Cadastro\n");
+                break;
+            case "P":
+                //Postar Mensagem referente à minha vontade
+                System.out.println("Postar\n");
+                break;
+            case "E":
+                //Excluir maluco
+                System.out.println("Exclui\n");
+                break;
+            case "R":
+                //Exibe o relatório
+                System.out.println("Exibe\n");
+                break;
+            case "S":
+                //Sair do programa
+                System.out.println("Sai\n");
+                this.executarSistema = false;
+                break;
+            default:
+                //Caso esteja fora do range de entradas
+                System.out.println("Digite um comando válido, por favor!");
+                break;
+        }
+    }
+    public void exibirMenu(){
         System.out.println("B3M-V1ND0 40 MAsK-S0c13ty! \n");
         System.out.println("Escolha sua ação:");
         System.out.println("(C)adastrar novo membro\n(P)ostar Mensagem\n(E)xcluir membro\n(R)elatório Geral\n(S)air do sistema");
-        System.out.println("\nHorário Atual: ");
+        System.out.println("\nHorário Atual: \n");
+    }
+    public void executar(){
+        String escolha;
+        while(executarSistema){
+            exibirMenu();
+            escolha = scanner.next().toUpperCase(Locale.ROOT);
+            opcao(escolha);
+        }
+    }
+
+    public Sistema() {
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in);
     }
 }
